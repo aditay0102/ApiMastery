78dbdd70838fb667bccd271d734efc16594ad496
aditay0102 <aditya0102.be21@chitkara.edu.in>
2023-11-03 06:34:20 -0800

    updated the final

--- src/Components/NewsApp/NewsApp.jsx
+++ src/Components/NewsApp/NewsApp.jsx
@@ -4,8 +4,10 @@ import axios from 'axios';
  function NewsApp() {
     const [news,setNews] = useState([]);
     
+    let api_key = "80a058a3570ab103577540c509368cf4";
+    let x = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=0a931d68eadd49408cd3db4fea612514";
     useEffect(()=>{
-        axios.get("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=0a931d68eadd49408cd3db4fea612514")
+        axios.get("https://gnews.io/api/v4/search?q=example&apikey=80a058a3570ab103577540c509368cf4")
         .then((res)=>{
             console.log(res.data.articles);
             setNews(res.data.articles);
@@ -27,7 +29,7 @@ import axios from 'axios';
                 
                 <div className='news_tile'>
                         <div className='imag_tile'>
-                            <img className='image' src={val.urlToImage} alt='' />
+                            <img className='image' src={val.image} alt='' />
                         </div>
                         <div className='data'>
                             <h3 className='title'>{val.title} 
